a55c5b769e8c75093d2ca2d2981d1198d7aaa75b
Du Duong <du.k.duong@vanderbilt.edu>
2022-11-05 12:02:48 -0500

    fix home screen

--- components/Home/index.tsx
+++ components/Home/index.tsx
@@ -1,23 +1,17 @@
-import React from 'react'
+import React from "react";
 
-import { Flex } from '@chakra-ui/react'
+import { Flex } from "@chakra-ui/react";
 
-import Header from './Header'
-import Widgets from './Widgets'
+import Header from "./Header";
+import Widgets from "./Widgets";
 
 const Home = () => {
   return (
-    <Flex
-      direction="column"
-      w='100%'
-      gap={16}
-      flex={1}
-      justifyContent='center'
-    >
-        <Header />
-        <Widgets />
+    <Flex direction="column" w="100%" gap={12} flex={1} justifyContent="center" p="">
+      <Header />
+      <Widgets />
     </Flex>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
